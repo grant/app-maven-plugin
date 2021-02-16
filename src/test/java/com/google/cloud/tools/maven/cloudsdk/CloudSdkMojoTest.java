8038f8216a1410f67627dad91f86d61fd67b6c51
Mridula <66699525+mpeddada1@users.noreply.github.com>
2021-02-16 11:12:30 -0500

    Increase test coverage for CloudSdkMojo (#441)
    * increase test coverage

--- src/test/java/com/google/cloud/tools/maven/cloudsdk/CloudSdkMojoTest.java
+++ src/test/java/com/google/cloud/tools/maven/cloudsdk/CloudSdkMojoTest.java
@@ -19,10 +19,13 @@ package com.google.cloud.tools.maven.cloudsdk;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.when;
 
+import java.util.Properties;
+import org.apache.maven.model.Plugin;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugin.descriptor.PluginDescriptor;
 import org.apache.maven.project.MavenProject;
+import org.codehaus.plexus.util.xml.Xpp3Dom;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -35,6 +38,8 @@ public class CloudSdkMojoTest {
   @Mock private PluginDescriptor pluginDescriptorMock;
 
   @Mock private MavenProject mavenProject;
+  @Mock private Properties properties;
+  @Mock private Plugin mockPlugin;
 
   @InjectMocks private CloudSdkMojoImpl mojo;
 
@@ -65,6 +70,31 @@ public class CloudSdkMojoTest {
     when(mavenProject.getPackaging()).thenReturn("this-is-a-test-packaging");
 
     assertEquals("this-is-a-test-packaging", mojo.getMavenProject().getPackaging());
+  }
+
+  @Test
+  public void testGetCompileMajorVersion_targetProperty() {
+    when(mavenProject.getProperties()).thenReturn(properties);
+    when(properties.getProperty("maven.compiler.target")).thenReturn("1.7");
+
+    assertEquals("1.7", mojo.getCompileTargetVersion());
+  }
+
+  @Test
+  public void testGetCompileMajorVersion_compilerPluginTarget() {
+    // maven.compiler.target property is null
+    when(mavenProject.getProperties()).thenReturn(properties);
+    when(properties.getProperty("maven.compiler.target")).thenReturn(null);
+
+    when(mavenProject.getPlugin("org.apache.maven.plugins:maven-compiler-plugin"))
+        .thenReturn(mockPlugin);
+    Xpp3Dom pluginConfiguration = new Xpp3Dom("configuration");
+    when(mockPlugin.getConfiguration()).thenReturn(pluginConfiguration);
+    Xpp3Dom compilerTarget = new Xpp3Dom("target");
+    pluginConfiguration.addChild(compilerTarget);
+    compilerTarget.setValue("1.8");
+
+    assertEquals("1.8", mojo.getCompileTargetVersion());
   }
 
   static class CloudSdkMojoImpl extends CloudSdkMojo {
